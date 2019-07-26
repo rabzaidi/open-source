eeaae9e320f40aa9ca91aa4633e46fea5130233f
rabzaidi <51254107+rabzaidi@users.noreply.github.com>
2019-07-26 15:52:57 +0530

    Add files via upload

---
+++ lab2rabab.py
@@ -0,0 +1,17 @@
+Python 3.4.1 (v3.4.1:c0e311e010fc, May 18 2014, 10:38:22) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print("hello")
+hello
+>>> 
+for x in range(3)
+SyntaxError: invalid syntax
+>>> x="hello world"
+>>> for x in range(3)
+SyntaxError: invalid syntax
+>>> 
+>>> for x inj range(3):
+	
+SyntaxError: invalid syntax
+>>> for x in range(3):
+	print(x)
+	
